c598d402b99efcd89665d4b73fd0c567737a46ff
Arian Shaikh <ariansk90@gmail.com>
2023-04-15 09:53:31 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,46 @@
-import React, { useState } from 'react';
-import './App.css';
-import NavBar from './components/UI/Nav/Nav';
-import HomeComponent from './components/Home/HomePage';
-import UploadComponent from './components/Upload/UploadComponent';
-import QnaComponent from './components/Qna/QnaComponent';
+import React, { useState } from "react";
+import "./App.css";
+import NavBar from "./components/UI/Nav/Nav";
+import HomeComponent from "./components/Home/HomePage";
+import UploadComponent from "./components/Upload/UploadComponent";
+import QnaComponent from "./components/Qna/QnaComponent";
 function App() {
   const [isUploadPage, setIsUploadPage] = useState(null);
   const [isQnaPage, setIsQnaPage] = useState(null);
 
-  const teachItButtonHandler = ()=>{
-    if(isUploadPage){
+  const teachItButtonHandler = () => {
+    if (isUploadPage) {
       setIsUploadPage(false);
-    }else{
+    } else {
       setIsUploadPage(true);
-    }
-  }
-  const askItButtonHandler = ()=>{
-    if(isQnaPage){
       setIsQnaPage(false);
-    }else{
-      setIsQnaPage(true);
     }
-  }
+  };
+  const askItButtonHandler = () => {
+    if (isQnaPage) {
+      setIsQnaPage(false);
+    } else {
+      setIsQnaPage(true);
+      setIsUploadPage(false);
+    }
+  };
 
   return (
     <div className="app">
-      <NavBar onTeachIt={teachItButtonHandler} onAskIt={askItButtonHandler}></NavBar>
-      {
-        isUploadPage ? (
-          <UploadComponent onGoBack={teachItButtonHandler}></UploadComponent>
-        ) : isQnaPage ? (
-          <QnaComponent onGoBack={askItButtonHandler}></QnaComponent>
-        ) : (
-          <HomeComponent onTeachIt={teachItButtonHandler} onAskIt={askItButtonHandler}></HomeComponent>
-        )
-      }
-      
+      <NavBar
+        onTeachIt={teachItButtonHandler}
+        onAskIt={askItButtonHandler}
+      ></NavBar>
+      {isUploadPage && (
+        <UploadComponent onGoBack={teachItButtonHandler}></UploadComponent>
+      )}
+      {isQnaPage && <QnaComponent onGoBack={askItButtonHandler}></QnaComponent>}
+      {!isUploadPage && !isQnaPage && (
+        <HomeComponent
+          onTeachIt={teachItButtonHandler}
+          onAskIt={askItButtonHandler}
+        ></HomeComponent>
+      )}
     </div>
   );
 }
