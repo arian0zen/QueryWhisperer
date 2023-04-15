8a9717489749fd694eb1e98e15e1f8c975c32265
Arian Shaikh <ariansk90@gmail.com>
2023-04-15 13:37:17 +0530

    changed fs dict

--- backend/app.js
+++ backend/app.js
@@ -4,23 +4,14 @@ const cors = require("cors");
 const app = express();
 const connectDB = require("./config/connectDB");
 const transScriptRouter = require("./routes/transScriptRouter");
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', '*');
-  res.header('Access-Control-Request-Headers', '*');
-  if (req.method === "OPTIONS") {
-    res.header('Access-Control-Allow-Methods', '*');
-    return res.status(200).json({});
-  }
-  next();
-});
+
 const corsOptions = {
-  origin: '*',
+  origin: 'https://transcript-generation-ft-ai-react.vercel.app',
    // Specify the allowed origin
   methods: 'GET, POST, PUT, DELETE', // Specify the allowed HTTP methods
   allowedHeaders: 'Content-Type, Authorization', // Specify the allowed headers
 };
-// app.use(cors(corsOptions));
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
