4b4ef28ed588a9813ec34b7309ac1cdfcb05c097
Arian Shaikh <ariansk90@gmail.com>
2023-04-15 11:07:37 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,9 @@ const connectDB = require("./config/connectDB");
 const transScriptRouter = require("./routes/transScriptRouter");
 
 const conrsOptions = {
-  origin: 'https://transcript-generation-ft-ai-react.vercel.app', // Specify the allowed origin
+  origin: 'https://transcript-generation-ft-ai-react.vercel.app',
+  Credentials: true, // Access-Control-Allow-Credentials: true
+   // Specify the allowed origin
   methods: 'GET, POST, PUT, DELETE', // Specify the allowed HTTP methods
   allowedHeaders: 'Content-Type, Authorization', // Specify the allowed headers
 };
