a4a47622ba35e6098508dae3395b07bcecb04e6f
Arian Shaikh <ariansk90@gmail.com>
2023-04-15 13:09:04 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ const corsOptions = {
   methods: 'GET, POST, PUT, DELETE', // Specify the allowed HTTP methods
   allowedHeaders: 'Content-Type, Authorization', // Specify the allowed headers
 };
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
