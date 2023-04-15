bb5800be0a8cc5ad206d2b0f3ac4ec7318accbbd
Arian Shaikh <ariansk90@gmail.com>
2023-04-15 11:45:11 +0530

    Update transcriptController.js

--- backend/controller/transcriptController.js
+++ backend/controller/transcriptController.js
@@ -3,51 +3,56 @@ const getTranscriptResponse = require("../api/getTranscript");
 const storeEmbeddedTranscript = require("../api/storeEmbeddedTranscript");
 
 const getTranscript = async (req, res) => {
-  const url =
-    req.body.url
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://transcript-generation-ft-ai-react.vercel.app"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  const url = req.body.url;
   const mp3 = "audio-output.mp3";
-    try{
-        const transcriptData = await getTranscriptResponse(url, mp3);
-        if (transcriptData.newFileCreated === true) {
-          fs.unlink(mp3, (err) => {
-            if (err) {
-              res.json({ error: "something went wrong" });
-              return;
-            }
-          });
-          fs.writeFile(
-            `./Documents/${transcriptData.mongoId}.txt`,
-            transcriptData.transcriptText,
-            (err) => {
-              if (err) {
-                res.json({ error: "something went wrong" });
-                return;
-              }
-            }
-          );
-          await storeEmbeddedTranscript(
-            `./Documents/${transcriptData.mongoId}.txt`
-          );
-        }
-        if (transcriptData.success === true) {
-            fs.unlink(`./Documents/${transcriptData.mongoId}.txt`, (err) => {
-                if (err) {
-                  return;
-                }
-              });
-          res.json({
-            transcriptData: transcriptData,
-            message: "now you can ask question about this",
-          });
-          return;
-        } else {
-          res.json({ error: transcriptData.error });
+  try {
+    const transcriptData = await getTranscriptResponse(url, mp3);
+    if (transcriptData.newFileCreated === true) {
+      fs.unlink(mp3, (err) => {
+        if (err) {
+          res.json({ error: "something went wrong" });
           return;
         }
-    }catch(error){
-        res.json({error: error.message});
-        return;
+      });
+      fs.writeFile(
+        `./Documents/${transcriptData.mongoId}.txt`,
+        transcriptData.transcriptText,
+        (err) => {
+          if (err) {
+            res.json({ error: "something went wrong" });
+            return;
+          }
+        }
+      );
+      await storeEmbeddedTranscript(
+        `./Documents/${transcriptData.mongoId}.txt`
+      );
     }
+    if (transcriptData.success === true) {
+      fs.unlink(`./Documents/${transcriptData.mongoId}.txt`, (err) => {
+        if (err) {
+          return;
+        }
+      });
+      res.json({
+        transcriptData: transcriptData,
+        message: "now you can ask question about this",
+      });
+      return;
+    } else {
+      res.json({ error: transcriptData.error });
+      return;
+    }
+  } catch (error) {
+    res.json({ error: error.message });
+    return;
+  }
 };
 
 module.exports = getTranscript;
