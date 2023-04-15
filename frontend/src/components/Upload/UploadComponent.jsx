6a0fe3e417eb7c0c08b3c24ab22f1e43818df1c5
Arian Shaikh <ariansk90@gmail.com>
2023-04-15 11:31:03 +0530

    Update UploadComponent.jsx

--- frontend/src/components/Upload/UploadComponent.jsx
+++ frontend/src/components/Upload/UploadComponent.jsx
@@ -42,7 +42,7 @@ const UploadComponent = (props) => {
         }
       };
       const response = await axios.post(
-        "https://transcript-generation-ft-ai.vercel.app/api/qna",
+        "https://transcript-generation-ft-ai.vercel.app/api/transcript",
         {
           url: enteredLink,
         },
